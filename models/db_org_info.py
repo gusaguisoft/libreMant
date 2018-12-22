94c529b60ead264af58c56823be47716f4777fa9
gusaguisoft <gusaguisoft@yahoo.com>
2018-12-22 12:55:56 -0300

    Se agrega modelo db_org_info (tablas equipos, puestos y secundarias)

---
+++ models/db_org_info.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# PUESTOS
+db.define_table('puestos'
+                ,Field('nombre', 'string', length=30, notnull=True, unique=True)
+                ,Field('idarea', 'reference areas', notnull=True, label='Area')
+                ,Field('idubicacion', 'reference ubicaciones', notnull=True, label='Ubicación')
+                ,Field('descripcion', 'string', length=150)
+                ,rname='info.puestos'
+                ,format='%(nombre)s')
+
+# PROPIETARIOS
+db.define_table('propietarios'
+                ,Field('nombre', 'string', length=75, notnull=True, unique=True)
+                ,Field('seudonimo', 'string', length=50, notnull=True, unique=True)
+                ,Field('abreviatura', 'string', length=25, notnull=True, unique=True)
+                ,Field('siglas', 'string', length=10, notnull=True, unique=True)
+                ,Field('descripcion', 'string', length=150)
+                ,Field('imagen', 'upload')
+                ,Field('activo', 'boolean')
+                ,rname='info.propietarios'
+                ,format='%(seudonimo)s')
+
+
+# TIPOS DE BIEN
+db.define_table('tiposbien'
+                ,Field('nombre', 'string', length=75, notnull=True, unique=True)
+                ,Field('seudonimo', 'string', length=50, notnull=True, unique=True)
+                ,Field('abreviatura', 'string', length=25, notnull=True, unique=True)
+                ,Field('siglas', 'string', length=10, notnull=True, unique=True)
+                ,Field('descripcion', 'string', length=150)
+                ,Field('imagen', 'upload')
+                ,Field('activo', 'boolean')
+                ,Field('componente', 'boolean', notnull=True)
+                ,rname='info.tiposbien'
+                ,format='%(seudonimo)s')
+
+# MARCAS
+db.define_table('marcas'
+                ,Field('nombre', 'string', length=75, notnull=True, unique=True)
+                ,Field('seudonimo', 'string', length=50, notnull=True, unique=True)
+                ,Field('abreviatura', 'string', length=25, notnull=True, unique=True)
+                ,Field('siglas', 'string', length=10, notnull=True, unique=True)
+                ,Field('descripcion', 'string', length=150)
+                ,Field('imagen', 'upload')
+                ,Field('activo', 'boolean')
+                ,rname='info.marcas'
+                ,format='%(seudonimo)s')
+
+# MODELOS
+db.define_table('modelos'
+                ,Field('idtipobien', 'reference tiposbien', notnull=True)
+                ,Field('idmarca', 'reference marcas', notnull=True)
+                ,Field('nombre', 'string', length=75, notnull=True, unique=True)
+                ,Field('seudonimo', 'string', length=50, notnull=True, unique=True)
+                ,Field('abreviatura', 'string', length=25, notnull=True, unique=True)
+                ,Field('siglas', 'string', length=10, notnull=True, unique=True)
+                ,Field('descripcion', 'string', length=150)
+                ,Field('imagen', 'upload')
+                ,Field('activo', 'boolean')
+                ,rname='info.modelos'
+                ,format='%(seudonimo)s')
+
+# EQUIPOS
+db.define_table('equipos'
+                ,Field('idmodelo', 'reference modelos', notnull=True, label='Modelo')
+                ,Field('matricula', 'string', length=25, notnull=True, unique=True, label='Matrícula')
+                ,Field('nroserie', 'string', length=25, label='N° Serie')
+                ,Field('descripcion', 'string', length=150)
+                ,Field('idpropietario', 'reference propietarios', notnull=True, label='Propietario')
+#                ,Field('idpuesto', 'reference puestos', label='N° Puesto')
+                ,rname='info.equipos'
+                ,format='%(matricula)s')
