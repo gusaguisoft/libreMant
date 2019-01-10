029335aaab148dc203f355c0bb701c1f5dbd74b3
gusaguisoft <gusaguisoft@yahoo.com>
2019-01-10 13:56:46 -0300

    Agregar equipos a puesto funcionando, Tablas tipossolicitud y solicitudes

--- controllers/taller.py
+++ controllers/taller.py
@@ -7,7 +7,7 @@ def puestos():
             db.puestos.nombre, \
             db.puestos.idarea, \
             db.puestos.idubicacion]
-    links=[lambda row: gridbuttonext("buttonprint","Equipos",URL("taller","equipospuesto",args=[row.id]))]
+    links=[lambda row: gridbuttonext("buttonprint","Equipos",URL("taller","equipos_puesto",args=[row.id]))]
     gridregs=SQLFORM.grid(db.puestos, fields=fields, formargs=formargs, createargs=createargs, links=links, user_signature=False)
     tabla=db.puestos
     backurl=URL('puestos')
@@ -20,12 +20,12 @@ def puestos():
     else:
         strtitulo = tabla._plural
         backurl=URL("default","index")
-    backbutton=gridbuttonext("buttonback", "Atrás", backurl)
+    backbutton=gridbuttonext("buttonback", "Volver", backurl)
     gridregs.append(backbutton)
     return dict(titulo=strtitulo, puestos=gridregs)
 
-def equipospuesto():
-    # request.args(0) es id de la solicitud
+def equipos_puesto():
+    # request.args(0) es el id del puesto
     puesto=db.puestos[request.args(0)]
     addbutton=gridbuttonext("buttonadd", "Agregar", URL('equipossinpuesto'))
     fields=[db.equipos.id, \
@@ -46,11 +46,13 @@ def equipospuesto():
                           maxtextlength=50, \
                           formargs=formargs,\
                           createargs=createargs,\
+                          csv=False, \
                           ui=ui)
-    addbutton=gridbuttonext("buttonadd", "Agregar Equipo", URL('equiposasignables',args=(request.args(0))))
-    gridregs.insert(0, addbutton)
+    addbutton=gridbuttonext("buttonadd", "Agregar Equipos", URL('equipos_sin_puesto',args=(request.args(0))))
+    #gridregs.insert(0, addbutton)
+    gridregs[0].insert(1, addbutton)
     tabla=db.equipos
-    backurl=URL('equipospuesto')
+    backurl=URL('equipos_puesto')
     if request.args(-2)=="new":
         strtitulo = "Nuevo " + tabla._singular + " del Puesto"
     elif request.args(-3)=="view":
@@ -60,13 +62,11 @@ def equipospuesto():
     else:
         strtitulo = tabla._plural + " del Puesto"
         backurl=URL("default","index")
-    backbutton=gridbuttonext("buttonback", "Atrás", backurl)
+    backbutton=gridbuttonext("buttonback", "Volver", backurl)
     gridregs.append(backbutton)
     return dict(titulo=strtitulo, puesto=puesto, equipospuesto=gridregs)
 
-def equiposasignables():
-    # request.args(0) es id de la solicitud
-    # puesto=db.puestos[request.args(0)]
+def equipos_sin_puesto():
     fields=[db.equipos.id, \
             db.equipos.idmodelo, \
             db.equipos.matricula, \
@@ -77,7 +77,7 @@ def equiposasignables():
                           fields=fields, \
                           args=request.args, \
                           searchable=True, \
-                          selectable=lambda ids:redirect(URL('equiposagregados',\
+                          selectable=lambda ids:redirect(URL('agregar_equipos_puesto',\
                                                              args=(request.args(0),'preguntar'),\
                                                              vars=dict(ids=ids))),
                           user_signature=False, \
@@ -85,31 +85,44 @@ def equiposasignables():
                           create=False,
                           deletable=False,
                           editable=False,
+                          csv=False,
                           ui=ui)
-    backbutton=gridbuttonext("buttonback", "Volver", URL('equipospuesto'))
+    backbutton=gridbuttonext("buttonback", "Volver", URL('equipos_puesto'))
     gridregs.append(backbutton)
     return dict(equiposasignables=gridregs)
 
-def equiposagregados():
-    # request.args(0) es id de la solicitud
+def agregar_equipos_puesto():
+    # request.args(0) es id del puesto
     puesto=db.puestos[request.args(0)]
     fields=[db.equipos.id, \
             db.equipos.idmodelo, \
             db.equipos.matricula, \
             db.equipos.nroserie]
-    query=(db.equipos.id.belongs(request.vars.ids))
+    if isinstance(request.vars.ids, list):
+        query=(db.equipos.id.belongs(request.vars.ids))
+    else:
+        query=(db.equipos.id==request.vars.ids)
     if request.args(1)=='hacer':
-        titulo="Equipos agregados a Puesto"
+        titulo="Equipos agregados al Puesto"
         for equipo in db(query).select():
             equipo.update_record(idpuesto=puesto.id)
     else:
-        titulo="Confirmación de Equipos a agregar al Puesto"
+        titulo="Agregar Equipos al Puesto"
     ui=myui()
     gridregs=SQLFORM.grid(query=query, fields=fields, args=request.args, searchable=False, create=False, deletable=False, editable=False,                      sortable=False, details=False, csv=False, ui=ui)
     if request.args(1)=='preguntar':
-        #confbutton=INPUT(_class="buttoncheck", _type="submit", _value="Confirmar")
-        confbutton=gridbuttonext("buttoncheck", "Confirmar", URL('equiposagregados',args=(puesto.id, 'hacer'),vars=request.vars))
+        confbutton=gridbuttonext("buttoncheck", "Confirmar", URL('agregar_equipos_puesto',args=(puesto.id, 'hacer'),vars=request.vars))
         gridregs.append(confbutton)
-    backbutton=gridbuttonext("buttonback", "Volver", URL('equipospuesto', args=(puesto.id)))
+    backbutton=gridbuttonext("buttonback", "Volver", URL('equipos_puesto', args=(puesto.id)))
     gridregs.append(backbutton)
-    return dict(titulo=titulo, puesto=puesto, equiposasignables=gridregs)
+    return dict(titulo=titulo, puesto=puesto, equipos=gridregs)
+
+def puestosx():
+    formargs=dict(submit_button='Aceptar', fields=['id', 'nombre', 'idarea', 'idubicacion', 'descripcion'])
+    createargs=dict(fields=['nombre', 'idarea', 'idubicacion', 'descripcion'])
+    fields=[db.puestos.id, \
+            db.puestos.nombre, \
+            db.puestos.idarea, \
+            db.puestos.idubicacion]
+    gridregs=SQLFORM.smartgrid(db.puestos, fields=fields, formargs=formargs, createargs=createargs, user_signature=False)
+    return dict(puestos=gridregs)
--- models/db_org_info.py
+++ models/db_org_info.py
@@ -75,3 +75,46 @@ db.define_table('equipos'
                 ,singular='Equipo'
                 ,plural='Equipos'
                 ,format='%(matricula)s')
+
+# TIPOS DE SOLICITUD
+db.define_table('tipossolicitud'
+                ,Field('nombre', 'string', length=75, notnull=True, unique=True)
+                ,Field('seudonimo', 'string', length=50, notnull=True, unique=True)
+                ,Field('abreviatura', 'string', length=25, notnull=True, unique=True)
+                ,Field('siglas', 'string', length=10, notnull=True, unique=True)
+                ,Field('descripcion', 'string', length=150)
+                ,Field('imagen', 'upload')
+                ,Field('activo', 'boolean')
+                ,Field('idsuperior', 'reference tiposolicitud', label='Superior')
+                ,rname='info.tipossolicitud'
+                ,format='%(seudonimo)s')
+db.tipossolicitud.idsuperior.represent = lambda v,r: str('' if v is None else r.idsuperior.nombre)
+db.tipossolicitud.idsuperior.requires = IS_EMPTY_OR(IS_IN_DB(db, 'tipossolicitud.id', '%(id)s - %(nombre)s'))
+db.tipossolicitud.imagen.requires = IS_EMPTY_OR(IS_IMAGE(extensions=('jpeg','png','bmp','gif'),maxsize=(200,200)))
+
+# SOLICITUDES
+db.define_table('solicitudes'
+                ,Field('fechasolicitud', 'date', notnull=True, required=False, label='Fecha Solicitud')
+                ,Field('horasolicitud', 'time', notnull=True, required=False, label='Hora Solicitud')
+                ,Field('idempleadosolicitante', 'reference empleados', notnull=True, label='Solicitado por')
+                ,Field('idareasolicitante', 'reference areas', notnull=True, label='Area Solicitante')
+                ,Field('idtiposolicitud', 'reference tipossolicitud', notnull=True, label='Tipo') 
+                ,Field('caratula', 'string', length=512, notnull=True, label='Carátula')
+                ,Field('estado', 'integer', notnull=True, default=0)
+                ,Field('fechaautorizacion', 'date', label='Fecha Autorización')
+                ,Field('horaautorizacion', 'time', label='Hora Autorización')
+                ,Field('idempleadoautorizacion', 'reference empleados', label='Autorizado por')
+                ,Field('prioridad', 'integer', notnull=True, default=0)
+                ,Field('comentarios', 'string', length=512)
+                ,rname='info.solicitudes'
+                ,migrate=True
+                ,format=lambda r: '('+str(r.id)+') - ' + rutacompleta(r.idareasolicitante) + ' - ' + rutacompleta(r.idtiposolicitud))
+# Los datos de los siguientes campos no los ingresa el usuario
+db.solicitudes.idareasolicitante.writable=False # se establece por CF según el área del usuario logeado con el rol adecuado
+db.solicitudes.idempleadosolicitante.writable=False # se establece por CF según el usuario logeado con el rol adecuado
+db.solicitudes.fechasolicitud.writable=False # la establece el servidor de BD
+db.solicitudes.horasolicitud.writable=False # la establece el servidor de BD
+db.solicitudes.idempleadoautorizacion.writable=False # se establece por CF según el usuario logeado con el rol adecuado
+db.solicitudes.fechaautorizacion.writable=False # la establece el servidor de BD
+db.solicitudes.horaautorizacion.writable=False # la establece el servidor de BD
+db.solicitudes.estado.writable=False # se establece por CF según la opción elegida
